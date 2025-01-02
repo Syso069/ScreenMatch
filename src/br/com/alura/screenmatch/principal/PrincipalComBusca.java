392580ecebc182e68654473f5607b98ba00e601a
Syso069 <sisomd69@gmail.com>
2025-01-02 16:41:23 -0300

    Adicionando a opção para o usuario digitar o nome do filme que a api ira buscar/ver

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner buscaFilme = new Scanner(System.in);
+        System.out.println("Digite o filme que deseja buscar: ");
+        var busca = buscaFilme.nextLine();
+
+        String retonaBuscaFilme = "http://www.omdbapi.com/?t=" + busca + "&apikey=c72c96bf";
+
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=c72c96bf"))
+                .uri(URI.create(retonaBuscaFilme))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
